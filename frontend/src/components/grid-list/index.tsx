04d633d0eeae452688cd48e62d54593380c51b59
xsirotak <469447@mail.muni.cz>
2024-11-19 00:01:01 +0100

    Added correct restaurant data loading into cards

--- frontend/src/components/grid-list/index.tsx
+++ frontend/src/components/grid-list/index.tsx
@@ -1,32 +1,42 @@
 import { Grid } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { RestaurantCard } from "../restaurant-card";
+import { restaurants } from "@/data/restaurants";
 
-const mockData = [
-  { name: "Item 1", description: "Description 1" },
-  { name: "Item 2", description: "Description 2" },
-  { name: "Item 3", description: "Description 3" },
-  { name: "Item 4", description: "Description 4" },
-  { name: "Item 5", description: "Description 5" },
-  { name: "Item 6", description: "Description 6" },
-  { name: "Item 7", description: "Description 7" },
-  { name: "Item 8", description: "Description 8" },
-  { name: "Item 9", description: "Description 5" },
-  { name: "Item 10", description: "Description 6" },
-  { name: "Item 11", description: "Description 7" },
-  { name: "Item 12", description: "Description 8" },
-  { name: "Item 13", description: "Description 5" },
-  { name: "Item 14", description: "Description 6" },
-  { name: "Item 15", description: "Description 7" },
-];
-
-// TODO: consume correct cards
 const GridList = () => {
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const data = await restaurants.getAllMetadata();
+        setRestaurantList(data);
+      } catch (err) {
+        setError("Failed to fetch restaurants. Please try again.");
+      }
+    };
+    fetchRestaurants();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
-    <Grid templateColumns="repeat(3, 1fr)" gap="6">
-      {mockData.map((_, index) => (
-        <RestaurantCard key={index} />
-      ))}
-    </Grid>
+      <Grid templateColumns="repeat(3, 1fr)" gap="6">
+        {restaurantList.map(({ id, name, categories, address, ratingAverage, priceCategory }) => (
+            <RestaurantCard
+                key={id}
+                id={id}
+                name={name}
+                categories={categories}
+                address={address}
+                ratingAverage={ratingAverage}
+                priceCategory={priceCategory}
+            />
+        ))}
+      </Grid>
   );
 };
 
--- frontend/src/components/restaurant-card/index.tsx
+++ frontend/src/components/restaurant-card/index.tsx
@@ -1,33 +1,35 @@
 import { Link } from "react-router-dom";
 import { Card } from "../card";
-import { Badge, Card as ChakraCard, Image } from "@chakra-ui/react";
+import {Badge, Card as ChakraCard, Image} from "@chakra-ui/react";
+import {FaStar} from "react-icons/fa6";
+import {FaDollarSign} from "react-icons/fa";
 
-const RestaurantCard = () => {
-  // TODO: restaurant id
+
+const RestaurantCard = ({ id, name, categories, address, ratingAverage, priceCategory }) => {
   return (
-    <Link to="/meals/id">
-      <Card>
-        <Image
-          src="https://cdn.apartmenttherapy.info/image/upload/v1644622714/k/Photo/Large%20Packages/2022-03-KESS-Tools/food-storage-glass-containers-horizontal.jpg"
-          height={150}
-        />
-
-        <ChakraCard.Body gap="1">
-          <ChakraCard.Title>Restaurant name</ChakraCard.Title>
-          <ChakraCard.Description>
-            This is the card body. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-            Curabitur nec odio vel dui euismod fermentum.
-          </ChakraCard.Description>
-        </ChakraCard.Body>
-        <ChakraCard.Footer justifyContent="flex-end">
-          <Badge>some</Badge>
-          <Badge>badges</Badge>
-          <Badge>attributes</Badge>
-        </ChakraCard.Footer>
-      </Card>
-    </Link>
+      <Link to={`/meals/${id}`}>
+        <Card>
+          <Image
+              src="https://cdn.apartmenttherapy.info/image/upload/v1644622714/k/Photo/Large%20Packages/2022-03-KESS-Tools/food-storage-glass-containers-horizontal.jpg"
+              alt={`${name} restaurant`}
+              height={150}
+          />
+          <ChakraCard.Body gap="1">
+            <ChakraCard.Title>{name}</ChakraCard.Title>
+            <ChakraCard.Description>{address}</ChakraCard.Description>
+          </ChakraCard.Body>
+          <ChakraCard.Footer justifyContent="flex-end">
+            {categories.map((category, index) => (
+                <Badge key={index}>{category}</Badge>
+            ))}
+            <Badge><FaStar color="yellow"/>{ratingAverage}</Badge>
+            <Badge> {Array.from({ length: priceCategory }).map((_, index) => (
+                <FaDollarSign color="green" key={index} />))}</Badge>
+          </ChakraCard.Footer>
+        </Card>
+      </Link>
   );
 };
 
 export { RestaurantCard };
+
