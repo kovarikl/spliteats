b7a871ce6e6f485275b41f46ff0a7a0d137b9aed
Lukáš Kovařík <kovariklukas@outlook.com>
2024-12-10 18:05:35 +0100

    Change API url to https

--- frontend/src/utils/const.ts
+++ frontend/src/utils/const.ts
@@ -1,6 +1,6 @@
 const DELIVERY_FEE = 49;
 const SERVICE_FEE = 29;
 
-const API_URL = "http://devserver-api.earcgroup.xyz/api";
+const API_URL = "https://devserver-api.earcgroup.xyz/api";
 
 export { DELIVERY_FEE, SERVICE_FEE, API_URL };
